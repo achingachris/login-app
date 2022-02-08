306d3b12bc92c08cad99ed7ff5fa55bacaeefc65
macbook <achinga.chris@gmail.com>
2022-02-08 15:14:28 +0300

    test-api-

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
-import express from 'express'
-import dotenv from 'dotenv'
-
-const port = process.env.PORT || 5000
+const express = require('express')
+const dotenv = require('dotenv').config()
 const app = express()
 
-dotenv.config()
+const port = process.env.PORT || 5500
+const enviroment = process.env.NODE_ENV || 'development'
 
-app.listen(port, () => console.log(`SERVER RUNNING ON PORT ${port}`))
+// SAMPLE API ROUTE
+app.get('/api/test', (req, res) => {
+  res.status(200).json({ message: 'Test API response working' })
+})
+
+// dotenv.config()
+app.listen(port, () =>
+  console.log(`SERVER RUNNING ON PORT ${port} in ${enviroment} mode`)
+)
