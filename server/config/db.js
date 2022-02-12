be7f43592ce2daca7de309ec2a874847728d87d2
macbook <achinga.chris@gmail.com>
2022-02-12 17:15:38 +0300

    clean code

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB connected: ${connection.connection.host}`.green.bold.underline)
+    console.log(
+      `MongoDB connected: ${connection.connection.host}`.green.bold.underline
+    )
   } catch (err) {
     console.log(`Error connecting to MongoDB: ${err.message}`.red.bold)
     process.exit(1)
