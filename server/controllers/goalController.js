12df341fd02b562696253612a82b024fea472776
macbook <achinga.chris@gmail.com>
2022-02-12 16:13:13 +0300

    goal-route-controllers

---
+++ server/controllers/goalController.js
@@ -0,0 +1,22 @@
+const getGoals = (req, res) => {
+  res.status(200).json({ message: 'get goals' })
+}
+
+const setGoals = (req, res) => {
+  res.status(200).json({ message: 'set goals' })
+}
+
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `update goal ${req.params.id}` })
+}
+
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `delete goal ${req.params.id}` })
+}
+
+module.exports = {
+  getGoals,
+  setGoals,
+  updateGoal,
+  deleteGoal,
+}
