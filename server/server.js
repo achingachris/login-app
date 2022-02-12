c444f9c553e3afb4710cbf5e996553a432a30e8c
macbook <achinga.chris@gmail.com>
2022-02-12 16:13:43 +0300

    server

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ app.get('/api/test', (req, res) => {
   res.status(200).json({ message: 'Test API response working' })
 })
 
+// API ROUTE FOR GETTING GOALS
+app.use('/api/goals', require('./routes/goals'))
+
 // dotenv.config()
 app.listen(port, () =>
   console.log(`SERVER RUNNING ON PORT ${port} in ${enviroment} mode`)
