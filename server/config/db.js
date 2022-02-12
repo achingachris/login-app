8521c2f2ca565c183cc9eadaae7cdace6b7442da
macbook <achinga.chris@gmail.com>
2022-02-12 17:14:09 +0300

    mongo-db-connection

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB connected: ${connection.connection.host}`.green.bold.underline)
+  } catch (err) {
+    console.log(`Error connecting to MongoDB: ${err.message}`.red.bold)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
