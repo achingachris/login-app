0b9ebfa424c2d0db1a24333796fb92507dda1ea4
macbook <achinga.chris@gmail.com>
2022-02-12 16:13:34 +0300

    routes-from-controller

--- server/routes/goals.js
+++ server/routes/goals.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const {
+  getGoals,
+  setGoals,
+  updateGoal,
+  deleteGoal,
+} = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'get goals' })
-})
+router.get('/', getGoals)
 
-router.post('/', (req, res) => {
-  res.status(200).json({ message: 'set goals' })
-})
+router.post('/', setGoals)
 
-router.put('/:id', (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` })
-})
+router.put('/:id', updateGoal)
 
-router.delete('/:id', (req, res) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` })
-})
+router.delete('/:id', deleteGoal)
 
 module.exports = router
