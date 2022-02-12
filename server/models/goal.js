ab545b330056f02841b2efd3890f582141049101
macbook <achinga.chris@gmail.com>
2022-02-12 19:21:17 +0300

    user-relation

--- server/models/goal.js
+++ server/models/goal.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const goalSchema = Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
   text: {
     type: String,
     required: [true, 'Goal text is required'],
