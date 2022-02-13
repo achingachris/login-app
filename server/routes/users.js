05117fcb1c74a9328b7166f799950c897c1fa1e0
macbook <achinga.chris@gmail.com>
2022-02-13 18:29:54 +0300

    suer

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser } = require('../controllers/userController')
+const {
+  registerUser,
+  authenticateUser,
+  getUserData,
+} = require('../controllers/userController')
 
 router.post('/', registerUser)
+router.post('/login', authenticateUser)
+router.get('/me', getUserData)
 
 module.exports = router
