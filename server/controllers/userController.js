2b630c2da370a7485cb01ed24f8ebc8e1358b417
macbook <achinga.chris@gmail.com>
2022-02-13 18:29:38 +0300

    user-api-route-controler

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,20 @@
+// register new user
 const registerUser = (req, res) => {
   res.json({ message: 'Registering user' })
 }
 
+// authenticate user
+const authenticateUser = (req, res) => {
+  res.json({ message: 'Authenticate user' })
+}
+
+// get user's data
+const getUserData = (req, res) => {
+  res.json({ message: 'Get user Data' })
+}
+
 module.exports = {
   registerUser,
+  authenticateUser,
+  getUserData,
 }
